15ed10ef98a1927b35045accf714ea80a69b7f95
nyacho04 <dnachod83@gmail.com>
2024-06-14 17:29:04 +0000

    commit

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int nya;
+
+	for (nya = 0; nya < 10; nya++)
+{
+		printf("%d", nya);
+}
+		printf("\n");
+
+		return (0);
+}
