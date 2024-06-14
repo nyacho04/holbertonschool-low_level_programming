695bd52bb505c82c1e937403e5ae433515bce751
nyacho04 <dnachod83@gmail.com>
2024-06-14 16:50:33 +0000

    commit

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{	char hola;
+	char HOLA;
+	for (hola = 'a'; hola <= 'z'; hola++)
+{
+	putchar(hola);
+}
+	for (HOLA = 'A'; HOLA <= 'z'; HOLA++)
+{
+	putchar(HOLA);
+}
+	putchar('\n');
+
+	return (0);
+}
