a4f41dc1af9b8fa5357c93d1d5a8dbfcd6d7b65d
nyacho04 <dnachod83@gmail.com>
2024-06-14 17:46:04 +0000

    commit

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{	int nya;
+
+	for (nya = 'z'; nya >= 'a'; nya--)
+{
+	putchar(nya);
+}
+		putchar('\n');
+		return (0);
+}
