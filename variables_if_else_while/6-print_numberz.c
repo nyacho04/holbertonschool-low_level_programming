1b0379264cb87ceddbb8ac333b694a993a592ed5
nyacho04 <dnachod83@gmail.com>
2024-06-14 17:39:16 +0000

    commit

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int nya;
+
+	for (nya = '0'; nya <= '9'; nya++)
+		putchar(nya);
+	putchar('\n');
+		return (0);
+}
