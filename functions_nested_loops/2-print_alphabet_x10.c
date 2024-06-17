81659d564cf1eda374eab4c29062f4d9d3c4daec
nyacho04 <dnachod83@gmail.com>
2024-06-17 15:21:02 +0000

    commit

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Check
+ * Description: function uses _putchar function to print
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char nya;
+	int nya2;
+
+	for (nya2 = 0; nya2 < 10; nya2++)
+	{
+		for (nya = 'a'; nya <= 'z'; nya++)
+			_putchar(nya);
+			_putchar('\n');
+	}
+}
+
