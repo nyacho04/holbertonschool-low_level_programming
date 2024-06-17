50489c6cbf27ab07c5031e6d741e86dbe971a64e
nyacho04 <dnachod83@gmail.com>
2024-06-17 14:37:05 +0000

    commit

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ * print_alphabet - Prints the alphabet in lowercase followed by a new line.
+ */
+void print_alphabet(void)
+{
+	char nya;
+
+	for (nya = 'a'; nya <= 'z'; nya++)
+{
+	_putchar(nya);
+}
+	_putchar('\n');
+
+}
