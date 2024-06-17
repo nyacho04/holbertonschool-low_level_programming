9ad71d6c3586f1e9b4a0cc3c5288b94854f9c17b
nyacho04 <dnachod83@gmail.com>
2024-06-17 16:29:26 +0000

    commit

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_abs - Check
+ * @nya: An integre input
+ * Description: This function returns absolute value of a number
+ * Return: Absolut value of number r
+*/
+int _abs(int nya)
+{
+
+	if (nya >= 0)
+		return (nya);
+	else
+		return (nya * -1);
+}
