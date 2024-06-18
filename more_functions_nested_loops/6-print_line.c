f584dd66749a9e42bf3edbf0bc59089aeac68ea8
nyacho04 <dnachod83@gmail.com>
2024-06-18 14:35:03 +0000

    commit

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - Draws a straight line using the character _.
+ * @n: The number of _ characters to be printed.
+ */
+void print_line(int n)
+{
+	int nya;
+
+if (n > 0)
+{
+	for (nya = 0; nya <= n; nya++)
+		_putchar('_');
+}
+	_putchar('\n');
+}
