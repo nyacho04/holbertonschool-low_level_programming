6b5d8df1f53c8d95ce5a7722bccd67cfafde2c71
nyacho04 <dnachod83@gmail.com>
2024-06-19 17:37:52 +0000

    commit

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - lelele
+ *
+ * @s: ueueue
+ *
+ */
+void print_rev(char *s)
+{
+	int nya;
+
+		for(nya = 0; s[nya]; nya++)
+		{
+			_putchar(s[nya]);
+		}
+		for(ayn = 0; s[ayn]; ayn++)
+		{
+			_putchar(s[ayn]);
+		}
+			_putchar('\n');
+}
