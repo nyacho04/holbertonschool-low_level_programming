027c67182786caef898cb7537d021e166650bb9e
nyacho04 <dnachod83@gmail.com>
2024-06-19 17:00:25 +0000

    commit

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - updates the value of a variable
+ *
+ * @n: value 98
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
