0cbbad43fc01d2518e0842de5786075bdefa4202
nyacho04 <dnachod83@gmail.com>
2024-06-20 12:28:27 +0000

    commit

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -10,14 +10,14 @@ void print_rev(char *s)
 	int nya, nya2, nya3;
 
 	nya = 0;
-		for (nya = 0; s[nya] != '\0'; nya++)
+		while (s[nya] != '0')
 		{
 			nya++;
 		}
 
 		nya3 = nya;
 
-		for (nya2 = nya3; s[nya2] >= 0; nya2--)
+		for (nya2 = nya3; s[nya2] != '0'; nya2--)
 		{
 			_putchar(s[nya2]);
 		}
