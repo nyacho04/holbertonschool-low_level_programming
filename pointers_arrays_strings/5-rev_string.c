d7e37e034d974588c4d99e890a2a30baeb01ad8e
nyacho04 <dnachod83@gmail.com>
2024-06-21 13:17:06 +0000

    commit

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,11 +1,12 @@
 #include "main.h"
-/*
- *  rev_string - ueueueueueu
- *  @s: mala noticia mi gente
- *  */
+/**
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ */
 void rev_string(char *s)
 {
-	char nya, nya2, nya3;
+	int nya, nya2, nya3;
+	char tk;
 
 	nya2 = 0;
 	nya3 = 0;
@@ -14,7 +15,12 @@ void rev_string(char *s)
 	{
 		nya2++;
 	}
-	nya3 = nya2 -1;
+	nya3 = nya2 - 1;
 
-	for (nya = 0; nya
-
+	for (nya = 0; nya < nya2 / 2; nya++)
+	{
+		tk = s[nya];
+		s[nya] = s[nya3];
+		s[nya3--] = tk;
+	}
+}
