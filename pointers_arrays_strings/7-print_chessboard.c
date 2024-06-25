18a670a9cc159b4950149b4296e2b67d3c2ea59e
nyacho04 <dnachod83@gmail.com>
2024-06-25 17:48:25 +0000

    commit

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,18 +1,19 @@
 #include "main.h"
+
 /**
- * print_chessboard - prints
- * @a: array
+ * print_chessboard - prints the chessboard
+ * @a: two dimension array to print
  */
 void print_chessboard(char (*a)[8])
 {
-	int i, b;
+	int i, j;
 
 	for (i = 0; i < 8; i++)
 	{
-		for (b = 0; b < 8; b++)
+		for (j = 0; j < 8; j++)
 		{
-			_putchar(a[i][b]);
+			_putchar(a[i][j]);
 		}
-		_putchar('\0');
+		_putchar('\n');
 	}
 }
