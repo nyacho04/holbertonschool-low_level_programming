205328ab754c80434a234d6f948cb20fb0c5d54e
nyacho04 <dnachod83@gmail.com>
2024-06-25 15:34:01 +0000

    commit

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *
+ */
+char *_strchr(char *s, char c)
+{
+	int a;
+	
+	while (1)
+	{
+		a = *s++;
+		if (a == c)
+		{
+			return (s -1);
+		}
+		if (a == 0)
+		{
+			return (NULL);
+		}
+	}
+}
