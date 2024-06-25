67fa84447517d97e06e210f118ce7b267aae445f
nyacho04 <dnachod83@gmail.com>
2024-06-25 17:44:29 +0000

    commit

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -3,6 +3,16 @@
 
 #include <unistd.h>
 
+void print_diagsums(int *a, int size);
+
+void print_chessboard(char (*a)[8]);
+
+char *_strstr(char *haystack, char *needle);
+
+char *_strpbrk(char *s, char *accept);
+
+unsigned int _strspn(char *s, char *accept);
+
 char *_strchr(char *s, char c);
 
 char *_memcpy(char *dest, char *src, unsigned int n);
