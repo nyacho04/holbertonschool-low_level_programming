1976de2dcbd409ef1b8a9e9f0d21ba29577aad63
nyacho04 <dnachod83@gmail.com>
2024-06-29 22:05:59 +0000

    commit

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints the name of the program
+ * @argv: array of arguments
+ * @argc
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+{
+	printf("%s\n", argv[0]);
+}
+	return (0);
+}
