1b256cb5ca86b7ead159a10e898a44e1b43d22f5
nyacho04 <dnachod83@gmail.com>
2024-07-02 03:06:37 +0000

    commit

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * *create_array - creates an array of chars
+ * @size: size of the array
+ * @c: char to c
+ *
+ * Return: pointer (Success), NULL (Error)
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *nya;
+
+	unsigned int i;
+
+	if (size == 0)
+
+	return (NULL);
+
+	nya = (char *) malloc(sizeof(char) * size);
+
+	if (nya == NULL)
+
+	return (0);
+
+	while (i < size)
+
+{
+	*(nya + i) = c;
+
+	i++;
+}
+	*(nya + i) = '\0';
+
+	return (nya);
+}
