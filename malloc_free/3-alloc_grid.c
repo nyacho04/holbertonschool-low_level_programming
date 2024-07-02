bd3f9d6b7d966f2c3d457c198ac4bfaea03be97f
nyacho04 <dnachod83@gmail.com>
2024-07-02 16:53:46 +0000

    commit

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * **alloc_grid - creatas an array of ints
+ * @width: width
+ * @height: height
+ * Return: (Success) or NULL (Error)
+ */
+int **alloc_grid(int width, int height)
+{
+	int **nya;
+	int a, b;
+
+	if (height <= 0 || width <= 0)
+		return (NULL);
+
+	nya = (int **) malloc(sizeof(int *) * height);
+
+	if (nya == NULL)
+	return (NULL);
+
+	for (a = 0; a < height; a++)
+{
+	nya[a] = (int *) malloc(sizeof(int) * width);
+	if (nya[a] == NULL)
+{
+	free(nya);
+	for (b = 0; b <= a; b++)
+		return (NULL);
+}
+	}
+
+	for (a = 0; a < height; a++)
+	{
+	for (b = 0; b < width; b++)
+		{
+	nya[a][b] = 0;
+	}
+}
+	return (nya);
+}
--- malloc_free/main.h
+++ malloc_free/main.h
@@ -5,3 +5,5 @@ char *_strdup(char *str);
 void free_grid(int **grid, int height);
 
 char *str_concat(char *s1, char *s2);
+
+int **alloc_grid(int width, int height);
