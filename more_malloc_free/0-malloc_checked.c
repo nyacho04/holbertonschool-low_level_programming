61a52580053c8a0c9f9686c389e6927360c33e72
nyacho04 <dnachod83@gmail.com>
2024-07-08 14:03:54 +0000

    commit

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * *malloc_checked - allocates memory with malloc
+ * @b: number of bytes
+ *
+ * Return: ptr
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+	exit(98);
+
+	return (ptr);
+}
