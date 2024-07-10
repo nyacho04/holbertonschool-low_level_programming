777c38152a9a27a588b1135b7777f69365723729
nyacho04 <dnachod83@gmail.com>
2024-07-10 18:00:57 +0000

    commit

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,57 @@
+#include "3-calc.h"
+/**
+ * op_add - calculates the sum of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of a and b
+ */
+int op_add(int a, int b);
+{
+	return (a + b);
+}
+/**
+ * op_sub - calculates the sum of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: rest of a and b
+ */
+int op_sub(int a, int b);
+{
+	return (a - b);
+}
+/**
+ * op_mul - calculates the sum of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: a and b
+ */
+int op_mul(int a, int b);
+{
+	return (a * b);
+}
+/**
+ * op_div - calculates the sum of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: a and b
+ */
+int op_div(int a, int b);
+{
+	return (a / b);
+}
+/**
+ * op_mod - calculates the sum of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: a and b
+ */
+int op_mod(int a, int b);
+{
+	return (a % b);
+}
+
