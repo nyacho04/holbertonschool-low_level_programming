2efa566ff3d1a159bcefbf8aefbf6d3c8843f84a
nyacho04 <dnachod83@gmail.com>
2024-07-10 04:26:23 +0000

    commit

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,86 +1,28 @@
 #include "dog.h"
-#include <stdio.h>
 #include <stdlib.h>
 /**
- * _nya - returns the length
- * @a: string
+ * new_dog - creates a new structure
+ * @name: name
+ * @age: age
+ * @owner: owner
  *
- * Return: the length
- */
-int _nya(char *a)
-{
-	int i;
-
-	i = 0;
-	while (a[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-/**
- * *_nyacpy - copies the string pointed to by src
- * @uno: pointer to the buffer
- * @dos: string to be copied
- *
- * Return: pointer to dest
- */
-char *_nyacpy(char *uno, char *dos)
-{
-	int tres, i;
-
-	tres = 0;
-
-	while (dos[tres] != '\0')
-	{		tres++;
-	}
-	i = 0;
-	while (i < tres)
-	{
-		uno[i] = dos[i];
-		i++;
-	}
-	uno[i] = '\0';
-
-	return (uno);
-}
-/**
- * new_dog - creates a new dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
- *
- * Return: pointer to the new dog (Success), NULL otherwise
+ * Return: pointer
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog;
-	int nya1;
-	int nya2;
+	struct dog *new_dog;
 
-	nya1 = 0;
-	nya2 = 0;
+	new_dog = malloc(sizeof(struct dog));
 
-	dog = malloc(sizeof(dog_t));
-	if (dog == NULL)
-		return (NULL);
-	dog->name = malloc(sizeof(char) * (nya1 + 1));
-	if (dog->name == NULL)
-	{
-		free(dog);
-		return (NULL);
-	}
-	dog->owner = malloc(sizeof(char) * (nya2 + 1));
-	{
-		free(dog);
-		free(dog->owner);
-		return (NULL);
-	}
-	_nyacpy(dog->name, name);
+	if (new_dog == NULL)
+{
+	free(new_dog);
 
-	_nyacpy(dog->owner, owner);
+	return (NULL);
+}
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
 
-	dog->age = age;
-
-	return (dog);
+	return (new_dog);
 }
