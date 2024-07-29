1ac1214e11cc31951e73c4b183d2bafc0920b31d
nyacho04 <dnachod83@gmail.com>
2024-07-29 17:03:09 +0000

    commit

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -14,8 +14,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new_node;
 
 	new_node = malloc(sizeof(dlistint_t));
-	if (!new_node)
+	if (new_node == NULL)
+	{
+
 		return (NULL);
+	}
 	new_node->n = n;
 	new_node->next = *head;
 	*head = new_node;
