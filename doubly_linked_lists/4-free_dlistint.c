ce53093fb591609ccfdb92787ade2562261ba129
nyacho04 <dnachod83@gmail.com>
2024-07-30 17:41:22 +0000

    commit

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -15,8 +15,10 @@ void free_dlistint(dlistint_t *head)
 	if (head != NULL)
 	{
 
-	for (; head->prev != NULL; head = head->prev)
-
+	while (head->prev != NULL)
+{
+	head = head->prev;
+}
 	while (head != NULL)
 	{
 		temp = head;
