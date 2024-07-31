189378b5e0d3d2dfd90997fcbf99390ede6342a3
nyacho04 <dnachod83@gmail.com>
2024-07-31 14:08:00 +0000

    commit

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * sum_dlistint - return the suma of the data in double linked list
+ *
+ * @head: head
+ * Return: suma data
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int suma;
+
+	suma = 0;
+
+	if (head != NULL)
+{
+	while (head->prev != NULL)
+		head = head->prev;
+	while (head != NULL)
+{
+		head = head->next;
+		suma += head->n;
+}
+}
+	return (suma);
+}
