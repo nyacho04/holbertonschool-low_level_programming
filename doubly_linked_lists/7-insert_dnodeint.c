bec70fd1bd5bbacb8b046769bbb158acdba2e9a5
nyacho04 <dnachod83@gmail.com>
2024-08-04 10:18:19 +0000

    commit

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -11,48 +11,44 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node;
-	dlistint_t *head;
-	dlistint_t *prev;
 	dlistint_t *new;
-
+	dlistint_t *head;
 	unsigned int i;
 
-
-	new_node = NULL;
+	new = NULL;
 	if (idx == 0)
-		new_node = add_dnodeint(h, n);
+		new = add_dnodeint(h, n);
 	else
 	{
-	head = *h;
-	i = 1;
-	if (head != NULL)
-		while (head->prev != NULL)
-			head = head->prev;
+		head = *h;
+		i = 1;
+		if (head != NULL)
+			while (head->prev != NULL)
+				head = head->prev;
 		while (head != NULL)
 		{
 			if (i == idx)
 			{
 				if (head->next == NULL)
-					new_node = add_dnodeint_end(h, n);
-				else
+				new = add_dnodeint_end(h, n);
+			else
+			{
+				new = malloc(sizeof(dlistint_t));
+				if (new != NULL)
 				{
-					new_node = malloc(sizeof(dlistint_t));
-					if (new_node != NULL)
-					{
-					new_node->n = n;
-					new_node->next = head->next;
-					new_node->prev = head;
-					head->next->prev = new;
-					head->next = prev;
-					}
-					}
-					break;
-					}
-					head = head->next;
-					i++;
+				new->n = n;
+				new->next = head->next;
+				new->prev = head;
+				head->next->prev = new;
+				head->next = new;
 				}
 			}
-			free (new_node);
-			return (new_node);
+			break;
+			}
+			head = head->next;
+			i++;
 		}
+	}
+
+	return (new);
+}
