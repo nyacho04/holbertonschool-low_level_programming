d951a27cdb7eb633aa6693ef59a17013a6e9c5d3
nyacho04 <dnachod83@gmail.com>
2024-10-22 13:05:36 -0300

    commit

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,8 +2,7 @@
 /**
  * main - Entry point of the program
  *
- * Description: This function prints a message to the standard output
- *              stating that "Programming is like building a multilingual puzzle".
+ * Description: This function prints a message.
  *
  * Return: Always returns 0 (success)
  */
