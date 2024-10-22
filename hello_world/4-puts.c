d7c9653f98642b8b444a2e8df7a7d0415476367b
nyacho04 <dnachod83@gmail.com>
2024-10-22 13:04:40 -0300

    commit

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints a message to the standard output
+ *              stating that "Programming is like building a multilingual puzzle".
+ *
+ * Return: Always returns 0 (success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
