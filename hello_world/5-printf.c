f82960b6b803317557624ad56977080bb387ecc4
nyacho04 <dnachod83@gmail.com>
2024-10-22 14:31:41 -0300

    commit

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints a message.
+ *
+ * Return: Always returns 0 (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
