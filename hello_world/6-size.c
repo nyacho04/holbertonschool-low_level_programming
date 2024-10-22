2a7ee73d1c4712f69deab92d75b7404e8d1c8b3c
nyacho04 <dnachod83@gmail.com>
2024-10-22 14:38:23 -0300

    commit

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,10 +8,11 @@
  */
 int main(void)
 {
-printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
-printf("Size of an int: %d byte(s)\n", (int) sizeof(int));
-printf("Size of a long int: %d byte(s)\n", (int) sizeof(long));
-printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
+	printf("Size of a char: %u byte(s)\n", sizeof(char));
+	printf("Size of an int: %u byte(s)\n", sizeof(int));
+	printf("Size of a long int: %u byte(s)\n", sizeof(long int));
+    printf("Size of a long long int: %u byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %u byte(s)\n", sizeof(float));
 
-    return 0;
+	return (0);
 }
