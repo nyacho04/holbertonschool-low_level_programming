3d18f37f6bec117f14098e16deebe4c44b738d67
nyacho04 <dnachod83@gmail.com>
2024-10-24 13:52:19 -0300

    commit

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: This function prints a message.
+*
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num + '0');
+		if (num != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
