4654d1d756cf5892e357b71443a9a2354c6ec265
nyacho04 <dnachod83@gmail.com>
2024-10-24 13:27:10 -0300

    commit

---
+++ variables_if_else_while/3-print_alphabets copy.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: This function prints a message.
+*
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char letra;
+
+	for  (letra = 'a'; letra <= 'z'; letra++)
+	{
+
+	if (letra != 'q' && letra != 'e')
+        {
+            putchar(letra);
+        }
+	}
+putchar('\n');
+
+return (0);
+}
