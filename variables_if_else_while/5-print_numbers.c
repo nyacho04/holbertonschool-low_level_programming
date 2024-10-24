b5c5daba50d743f2e5beaae92dc012f89b01e913
nyacho04 <dnachod83@gmail.com>
2024-10-24 13:39:27 -0300

    commit

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: This function prints a message.
+*
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+	printf("\n");
+
+	return (0);
+}
