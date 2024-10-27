a3449aa15bf04284c498b3592606b53ebef70665
nyacho04 <dnachod83@gmail.com>
2024-10-26 22:13:50 -0300

    commit

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_alphabet - This function prints a message.
+*
+* Return: Always 0 (success)
+*/
+void print_alphabet(void)
+{
+char letra;
+
+for (letra = 'a'; letra <= 'z'; letra++)
+{
+_putchar(letra);
+}
+_putchar('\n');
+}
