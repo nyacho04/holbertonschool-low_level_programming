470cce3e0c0c953deb292a0adce981d16cc8e12d
nyacho04 <dnachod83@gmail.com>
2024-10-26 21:54:54 -0300

    commit

---
+++ functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int _putchar(char c);
