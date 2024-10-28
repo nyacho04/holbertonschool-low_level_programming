a12e08e48837e9b94b4e1ffb7ca01122d641e8b0
nyacho04 <dnachod83@gmail.com>
2024-10-28 11:52:43 -0300

    commit

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* mul - This function prints a message.
+*
+* @a: The character to be checked.
+*
+* @b: The character to be checked.
+*
+* Return: 1 if c is uppercase, 0 otherwise.
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
