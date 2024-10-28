a744b01efa1fe7248012be701bac6efe325bcb5f
nyacho04 <dnachod83@gmail.com>
2024-10-27 22:27:44 -0300

    commit

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_sign- This function prints a message.
+*
+* @n: The character to be checked
+*
+* Return: Always 0 (success)
+*/
+int print_sign(int n);
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
