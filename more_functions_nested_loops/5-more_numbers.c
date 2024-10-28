1f208d7f9c0ad3283caf6cfb6f0a353ea6898d97
nyacho04 <dnachod83@gmail.com>
2024-10-28 14:31:44 -0300

    commit

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* more_numbers - This function prints a message.
+*
+* Return: Always 0 (Success)
+*/
+void more_numbers(void)
+{
+	int i, num;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			if (num > 9)
+				_putchar((num / 10) + '0');
+			_putchar((num % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
