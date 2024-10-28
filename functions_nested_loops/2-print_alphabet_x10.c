32973cc9938be10d37892628c8629b749888087f
nyacho04 <dnachod83@gmail.com>
2024-10-27 21:56:30 -0300

    commit

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_alphabet - This function prints a message.
+*
+* Return: Always 0 (success)
+*/
+void print_alphabet(void)
+{
+int i;
+char letra;
+
+    for (i = 0; i < 10; i++)
+
+    for (letra = 'a'; letra <= 'z'; letra++)
+    {
+    _putchar(letra);
+    }
+    _putchar('\n');
+}
