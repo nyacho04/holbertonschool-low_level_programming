2bad3fca6e6f1c07273a5cb8df074cb89ee0f65a
nyacho04 <dnachod83@gmail.com>
2024-10-29 14:45:24 -0300

    commit

---
+++ hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+* main - Entry point of the program
+*
+* Description: This function the size of various types on the computer.
+*
+* Return: Always returns 0 (success)
+*/
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+	return (1);
+}
