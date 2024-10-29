adba0f1de130454342451ba8ef9190ea15f5ef4d
nyacho04 <dnachod83@gmail.com>
2024-10-29 14:34:38 -0300

    commit

---
+++ more_functions_nested_loops/101-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* main - This function prints a message.
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	print_number(98);
+	_putchar('\n');
+	print_number(-402);
+	_putchar('\n');
+	print_number(1024);
+	_putchar('\n');
+	print_number(0);
+	_putchar('\n');
+	print_number(-987);
+	_putchar('\n');
+	return (0);
+}
