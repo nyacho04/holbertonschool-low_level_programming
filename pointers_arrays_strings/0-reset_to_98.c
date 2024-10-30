ff9f053daa4e875f15a455e19bad1b390f609ff9
nyacho04 <dnachod83@gmail.com>
2024-10-30 11:39:00 -0300

    commit

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* reset_to_98 - Entry point
+*
+* @n: pointer to an integer
+*
+* Description: This function prints a message.
+*
+* Return: Always 0 (success)
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
