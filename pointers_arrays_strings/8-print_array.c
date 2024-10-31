daa850e4163ae44107a84938c9b19c79df03bf66
nyacho04 <dnachod83@gmail.com>
2024-10-31 11:01:51 -0300

    commit

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/**
+* print_array - Entry point
+*
+* @a: array of integers
+*
+* @n: pointer to an integer
+*
+* Description: This function prints a message.
+*
+* Return: Always 0 (success)
+*/
+
+void print_array(int *a, int n)
+{
+	int nya;
+
+	for (nya = 0; nya < n; nya++)
+	{
+		printf("%d", a[nya]);
+		if (nya < n - 1)
+		printf(", ");
+	}
+		printf("\n");
+}
