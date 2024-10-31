69b3b3816f632a58cb8ad5149cee78748b83172b
nyacho04 <dnachod83@gmail.com>
2024-10-31 13:24:02 -0300

    commit

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -2,28 +2,28 @@
 #include <unistd.h>
 #include "main.h"
 /**
-* *_strcpy - Entry point
+* *_atoi - Entry point
 *
-* @dest: array of integers
-*
-* @src: pointer to an integer
+* @s: string to convert
 *
 * Description: This function prints a message.
 *
 * Return: Always 0 (success)
 */
 
-int _atoi(char *s) 
+int _atoi(char *s)
 {
-    int resultado = 0;
+	int resultado = 0;
 	int nya = 1;
 
-    for (; *s && (*s < '0' && *s > '9'); s++)
-        if (*s == '-')
-        nya = -nya;
-
-    for (; *s >= '0' && *s <= '9'; s++)
-        resultado = resultado * 10 + (*s - '0');
-
-    return (resultado * nya);
+	for (; *s && (*s < '0' && *s > '9'); s++)
+	{
+		if (*s == '-')
+		nya = -nya;
+	}
+	for (; *s >= '0' && *s <= '9'; s++)
+	{
+		resultado = resultado * 10 + (*s - '0');
+	}
+	return (resultado * nya);
 }
