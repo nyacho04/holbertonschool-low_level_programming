fe17ebec21a7b78619189ae1f37a667f534b72d1
nyacho04 <dnachod83@gmail.com>
2024-11-05 14:53:28 -0300

    commit

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -14,34 +14,14 @@
 void print_chessboard(char (*a)[8])
 {
 	int i;
+
 	int b;
 
 	for (i = 0; i < 8; i++)
 	{
 		for (b = 0; b < 8; b++)
 		{
-			if (i % 2 == 0)
-			{
-				if (b % 2 == 0)
-				{
-				printf("%c", a[i][b]);
-				}
-					else
-				{
-				printf(" ");
-				}
-					}
-					else
-					{
-				if (b % 2 == 0)
-				{
-					printf(" ");
-				}
-					else
-				{
-					printf("%c", a[i][b]);
-				}
-			}
+			printf("%c", a[i][b]);
 		}
 		printf("\n");
 	}
