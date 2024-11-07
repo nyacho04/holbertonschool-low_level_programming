88ea0ca15d22347d9785964c1ea27bd26ffa1086
nyacho04 <dnachod83@gmail.com>
2024-11-07 12:41:34 -0300

    task 8

---
+++ recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* factorial - This function prints a message.
+*
+* @n: The string to be printed.
+*
+* Return: 1 if c is uppercase, 0 otherwise.
+*/
+
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+		else
+		{
+			return (n * factorial(n - 1));
+		}
+}
