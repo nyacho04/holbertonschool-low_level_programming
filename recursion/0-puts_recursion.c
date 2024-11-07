dc40f26b599a3bc04fd734c32e24b1bc26c17d97
nyacho04 <dnachod83@gmail.com>
2024-11-07 10:45:22 -0300

    task 8

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* _puts_recursion - This function prints a message.
+*
+* @s: The string to be printed.
+*
+* Return: 1 if c is uppercase, 0 otherwise.
+*/
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
