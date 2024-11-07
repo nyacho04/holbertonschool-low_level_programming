a0265605d419d9b513c9de16b4b99aaaaefee3d9
nyacho04 <dnachod83@gmail.com>
2024-11-07 12:40:03 -0300

    task 8

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* _strlen_recursion - This function prints a message.
+*
+* @s: The string to be printed.
+*
+* Return: 1 if c is uppercase, 0 otherwise.
+*/
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+		else
+		{
+			return (1 + _strlen_recursion(s + 1));
+		}
+}
