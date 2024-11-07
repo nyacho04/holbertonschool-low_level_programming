b86c40ae39f954ccee736caec580133a25806a18
nyacho04 <dnachod83@gmail.com>
2024-11-07 13:43:35 -0300

    task 8

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* is_prime_number - This function prints a message.
+*
+* @n: The number to be printed.
+*
+* Return: 1 if c is uppercase, 0 otherwise.
+*/
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	if (n <= 3)
+	{
+		return (1);
+	}
+	if (n % 2 == 0 || n % 3 == 0)
+	{
+		return (0);
+	}
+	return (is_prime_number(n / 2));
+}
