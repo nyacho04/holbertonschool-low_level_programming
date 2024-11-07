10f53e55dff54fa39fe11ff619ec1371d98e1487
nyacho04 <dnachod83@gmail.com>
2024-11-07 10:20:38 -0300

    "Modified print_diagsums function to print diagonal sums on the same line separated by a comma."

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -24,6 +24,5 @@ void print_diagsums(int *a, int size)
 		sum1 += a[i * size + i];
 		sum2 += a[i * size + (size - i - 1)];
 	}
-	printf("%d\n", sum1);
-	printf("%d\n", sum2);
+	printf("%d, %d\n", sum1, sum2);
 }
