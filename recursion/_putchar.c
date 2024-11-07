ee9c9e5aa4fc7bf03a80e7ab5079ba1c0d8457da
nyacho04 <dnachod83@gmail.com>
2024-11-07 12:43:56 -0300

    task 8

--- recursion/_putchar.c
+++ recursion/_putchar.c
@@ -1,6 +1,6 @@
 #include <unistd.h>
 /** this function _putchar works like printf, but it prints a char */
-int _putchar(char *s)
+int _putchar(char *c)
 {
-	return (write(1, &s, 1));
+	return (write(1, &c, 1));
 }
