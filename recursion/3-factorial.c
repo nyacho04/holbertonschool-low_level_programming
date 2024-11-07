2555f580fc656cadd48313ce4df04c8ca46c9674
nyacho04 <dnachod83@gmail.com>
2024-11-07 12:43:17 -0300

    task 8

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -10,12 +10,16 @@
 
 int factorial(int n)
 {
+	if (n < 0)
+	{
+		return (-1);
+	}
 	if (n == 0)
 	{
 		return (1);
 	}
-		else
-		{
-			return (n * factorial(n - 1));
-		}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
 }
