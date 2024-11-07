1d5edaed3caecd35ba3faf25a6f99e513a19719c
nyacho04 <dnachod83@gmail.com>
2024-11-07 14:04:57 -0300

    "Updated VSCode settings: added compiler path and file association for main.h"

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - This function prints a message.
+*
+* @argc: The number of arguments passed to the program.
+*
+* @argv: An array of strings containing the arguments passed to the program.
+*
+* Return: 1 if c is uppercase, 0 otherwise.
+*/
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
