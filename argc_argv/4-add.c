60fbb31ddde9101630bb0f3e1d1f20d20efac783
nyacho04 <dnachod83@gmail.com>
2024-11-09 03:31:07 -0300

    "Updated 4-add.c to validate input arguments and modified add executable"

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -14,27 +14,28 @@
 int main(int argc, char *argv[])
 {
 	int i;
+	int n;
 	int sum = 0;
-	int num;
 
 	if (argc == 1)
 	{
 		printf("0\n");
 		return (0);
 	}
-
 	for (i = 1; i < argc; i++)
 	{
-		num = atoi(argv[i]);
-
-		if (num == 0 && *argv[i] != '0')
+		for (n = 0; argv[i][n] != '\0'; n++)
 		{
-			printf("Error\n");
-			return (1);
+			if (argv[i][n] < '0' || argv[i][n] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
-		sum += num;
+		sum += atoi(argv[i]);
 	}
 
 	printf("%d\n", sum);
+
 	return (0);
 }
