ebf530dcccefb04d9c2854130614514739afef22
nyacho04 <dnachod83@gmail.com>
2024-11-08 21:25:08 -0300

    commit

---
+++ argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - This function prints a message.
+*
+* @argc: The number of arguments passed to the program.
+*
+* @argv: An array of strings containing the arguments passed to the program.
+*
+* Return: 1 if c is uppercase, 0 otherwise.
+*/
+
+int main(int argc, char *argv[]) 
+{  
+    int i;
+    
+    for (i = 0; i < argc; i++)
+	{
+    	printf("%s\n", argv[i]);
+    }
+    return (0);
+}
