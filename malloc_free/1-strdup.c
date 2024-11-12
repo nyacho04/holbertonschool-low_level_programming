918c89286f0f7b71db5d9955f67a96db09b18bea
nyacho04 <dnachod83@gmail.com>
2024-11-12 11:15:21 -0300

    Added "#include "main.h"" to three C files and implemented the str_concat function in 2-str_concat.c.

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 /**
 * *_strdup - This function prints a message.
 *
--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -11,3 +11,46 @@
 * Return: A pointer to the array.
 */
 
+char *str_concat(char *s1, char *s2)
+{
+	char *concat;
+
+	unsigned int i = 0, n = 0, nya = 0, nya2 = 0;
+
+	if (s1 != NULL)
+	{
+		while (s1[nya] != '\0')
+		{
+			nya++;
+		}
+	}
+	if (s2 != NULL)
+	{
+		while (s2[nya2] != '\0')
+		{
+			nya2++;
+		}
+	}
+
+	concat = malloc((nya + nya2 + 1) * sizeof(char));
+
+	if (concat == NULL)
+	{
+		return (NULL);
+	}
+
+	if (s1 != NULL)
+	{
+		for (i = 0; i < nya; i++)
+			concat[i] = s1[i];
+	}
+	if (s2 != NULL)
+	{
+		for (n = 0; n < nya2; n++)
+			concat[i + n] = s2[n];
+	}
+		concat[i + n] = '\0';
+
+	return (concat);
+
+}
