f1c675f8fd266e198d5299e6480baf3d48666c8c
nyacho04 <dnachod83@gmail.com>
2024-11-12 10:30:41 -0300

    Added null check for allocated memory and error message in create_array function.

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -23,6 +23,12 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 
+	if (array == NULL)
+	{
+		printf("failed to allocate memory\n");
+		return (NULL);
+	}
+
 	for (i = 0; i < size; i++)
 	{
 		array[i] = c;
