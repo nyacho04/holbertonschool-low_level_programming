e4e63819a6516b5299899c7faeb34d9a8dd5ffaf
nyacho04 <dnachod83@gmail.com>
2024-11-12 11:14:57 -0300

    commit

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+* *str_concat - This function prints a message.
+*
+* @s1: The first string to be printed.
+*
+* @s2: The second string to be printed.
+*
+* Return: A pointer to the array.
+*/
+
