96f1ef7e1531045a2d589ef2371ae72f5bb0f796
nyacho04 <dnachod83@gmail.com>
2024-11-12 10:22:44 -0300

    commit

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* *create_array - This function prints a message.
+*
+* @c: The size of the array.
+*
+* @size: The size of the array.
+*
+* Return: A pointer to the array.
+*/
+
+
