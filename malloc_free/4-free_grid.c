17efc6b06ac32d85be1f494c12517bcc658c7551
nyacho04 <dnachod83@gmail.com>
2024-11-12 13:32:23 -0300

    "Added function free_grid to free a 2D grid of integers"

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -10,3 +10,19 @@
 * Return: Nothing.
 */
 
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL)
+	return;
+
+	if (height == 0)
+	return;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+		free(grid);
+}
