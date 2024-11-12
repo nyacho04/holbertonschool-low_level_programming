6724489dff79b06dbf99debcf9422b654b892c18
nyacho04 <dnachod83@gmail.com>
2024-11-12 10:23:00 -0300

    "Added function create_array to dynamically allocate and initialize a character array with a specified size and character."

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -10,4 +10,21 @@
 * Return: A pointer to the array.
 */
 
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int i;
 
+	array = malloc(size * sizeof(char));
+
+	if (array == NULL)
+	{
+	return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
+		return (array);
+}
