3fdb68607476c7cdaf6f92432c8c90c280d03418
nyacho04 <dnachod83@gmail.com>
2024-11-12 13:32:08 -0300

    commit

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* free_grid - This function prints a message.
+*
+* @grid: The grid to be printed.
+*
+* @height: The height of the grid.
+*
+* Return: Nothing.
+*/
+
