f2e3972fbd79404fcf1dc6d014572de81970db3a
nyacho04 <dnachod83@gmail.com>
2024-11-12 12:57:50 -0300

    commit

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -11,3 +11,38 @@
 */
 
 int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i;
+	int n;
+
+	if (width <= 0)
+		return (NULL);
+
+	if (height <= 0)
+		return (NULL);
+
+	grid = malloc(height * sizeof(int *));
+
+	if (grid == NULL)
+		return (NULL);
+
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(width * sizeof(int));
+
+		if (grid[i] == NULL)
+		{
+			for (n = 0; n < i; n++)
+				free(grid[n]);
+				free(grid);
+
+			return (NULL);
+		}
+		for (n = 0; n < width; n++)
+			grid[i][n] = 0;
+	}
+
+	return (grid);
+}
