5d8eac18b16b5f591a483a18f7d6eb45a784cf37
nyacho04 <dnachod83@gmail.com>
2024-11-14 10:25:38 -0300

    commit

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* *malloc_checked - This function prints a message.
+*
+* @b: The number of bytes to allocate.
+*
+* Return: A pointer to the array.
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+		return (ptr);
+}
