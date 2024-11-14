f7d746c40fa1f70634d9d8f8776268da6f1960e1
nyacho04 <dnachod83@gmail.com>
2024-11-14 10:57:19 -0300

    commit

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+* *string_nconcat - This function prints a message.
+*
+* @s1: The first string.
+*
+* @s2: The second string.
+*
+* @n: The number of bytes to print from s2.
+*
+* Return: A pointer to the array.
+*/
+
+
