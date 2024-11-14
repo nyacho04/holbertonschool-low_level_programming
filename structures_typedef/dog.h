6708be60e68adb5ceaaf35f9736e3a8752b59d20
nyacho04 <dnachod83@gmail.com>
2024-11-14 14:02:39 -0300

    commit

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,20 +1,15 @@
-#include <stdio.h>
-#include "dog.h"
-
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-
-int main(void)
+* struct dog - Define a dog's attributes.
+* @name: Pointer to the dog's name.
+* @age: Dog's age as a float.
+* @owner: Pointer to the owner's name.
+*/
+struct dog
 {
-    struct dog my_dog;
+	char *name;
 
-    my_dog.name = "Poppy";
-    my_dog.age = 3.5;
-    my_dog.owner = "Bob";
+	float age;
 
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-    return (0);
-}
+	char *owner;
+
+};
