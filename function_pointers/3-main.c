9935a8ea147db4e10baf7f71e48ad73140bccfc6
nyacho04 <dnachod83@gmail.com>
2024-11-20 12:53:03 -0300

    commit

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,30 +1,42 @@
 #include "3-calc.h"
 #include <stdlib.h>
 #include <stdio.h>
+/**
+* main - Entry point, performs simple operations.
+*
+* @argc: Number of arguments.
+*
+* @argv: Array of arguments.
+*
+* Return: 0 on success.
+*/
 
 int main(int argc, char *argv[])
 {
-    int num1, num2, result;
-    int (*operation)(int, int);
+int num1;
+int num2;
+int resultado;
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }
+	int (*operacion)(int, int);
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[3]);
-    operation = get_op_func(argv[2]);
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-    if (!operation)
-    {
-        printf("Error\n");
-        exit(99);
-    }
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	operacion = get_op_func(argv[2]);
 
-    result = operation(num1, num2);
-    printf("%d\n", result);
+	if (!operacion)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-    return (0);
+	resultado = operacion(num1, num2);
+	printf("%d\n", resultado);
+
+	return (0);
 }
