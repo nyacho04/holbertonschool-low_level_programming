dc18c7bc8cc5f2ba8e6c965648a516f9e53db048
nyacho04 <dnachod83@gmail.com>
2024-11-21 12:01:03 -0300

    commit

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,30 @@
+#include <stdarg.h>
+#include "main.h"
+/**
+* sum_them_all - This function prints a message.
+*
+* @n: The number of integers passed to the function.
+*
+* @...: A variable number of integers to be printed.
+*
+* Return: The sum of all the integers passed to the function.
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int suma = 0;
+	va_list args;
+
+	if (n == 0)
+	return (0);
+
+va_start(args, n);
+
+for (i = 0; i < n; i++)
+suma += va_arg(args, int);
+
+va_end(args);
+
+return (suma);
+
+}
