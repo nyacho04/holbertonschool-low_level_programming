c65ef929d3fa2c13197f34e9bddf590953691010
nyacho04 <dnachod83@gmail.com>
2024-12-03 20:55:45 -0300

    singly linked list, first task done, lists.h file

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+* print_list - prints all elements of a list
+*
+* @h: pointer to the head of the list
+*
+* Return: size_t
+*/
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
