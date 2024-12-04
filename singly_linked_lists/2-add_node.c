0497abe0d218d17d700e06590edace7e3a4d9e77
nyacho04 <dnachod83@gmail.com>
2024-12-03 21:23:04 -0300

    commit

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -16,28 +16,18 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 
-	if (head == NULL || str == NULL)
-	return (NULL);
-
 	new_node = malloc(sizeof(list_t));
-
 	if (new_node == NULL)
-	return (NULL);
-
+	{
+		return (NULL);
+	}
 	new_node->str = strdup(str);
-
 	if (new_node->str == NULL)
 	{
 		free(new_node);
-
 		return (NULL);
 	}
 
-	while (str_copy[len] != '\0')
-		len++;
-
-	new_node->str = str_copy;
-	new_node->len = len;
 	new_node->next = *head;
 	*head = new_node;
 
