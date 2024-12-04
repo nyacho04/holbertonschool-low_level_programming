8b9cc499a202feb8db0cfc74d52b6b22e030f6e6
nyacho04 <dnachod83@gmail.com>
2024-12-03 21:17:20 -0300

    modified lists.h to add a function prototype for task. add_node done

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+* add_node - adds a new node at the end of a list
+*
+* @head: the head of the list
+*
+* @str: the string to be added
+*
+* Return: the address of the new node, or NULL if it failed
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	if (head == NULL || str == NULL)
+	return (NULL);
+
+	new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+	return (NULL);
+
+	new_node->str = strdup(str);
+
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+
+		return (NULL);
+	}
+
+	new_node->len = strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
