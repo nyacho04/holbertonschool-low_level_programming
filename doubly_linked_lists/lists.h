93358a1403e7ff4f4441a6cdf57aecda96b92a6e
nyacho04 <dnachod83@gmail.com>
2024-12-05 13:03:12 -0300

    first task for double linked list

---
+++ doubly_linked_lists/lists.h
@@ -0,0 +1,20 @@
+#ifndef LISTS_H
+#define LISTS_H
+/**
+ * struct dlistint_s - Doubly linked list node
+ * @n: Integer data
+ * @prev: Pointer to the previous node
+ * @next: Pointer to the next node
+ */
+
+typedef struct dlistint_s
+{
+	int n;
+
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h);
+
+#endif
