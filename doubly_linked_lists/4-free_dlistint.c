807ded1f58e9bb74f0145fb03b4485f94913bcb8
nyacho04 <dnachod83@gmail.com>
2024-12-07 14:01:58 -0300

    commit

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -2,30 +2,22 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
-
 /**
-* main - check the code
+* free_dlistint - frees a doubly linked list
 *
-* Return: Always EXIT_SUCCESS.
+* @head: the head of the doubly linked list
+*
+* Return: void
 */
-int main(void)
-{
-	dlistint_t *head;
-	dlistint_t *node;
 
-	head = NULL;
-	add_dnodeint_end(&head, 0);
-	add_dnodeint_end(&head, 1);
-	add_dnodeint_end(&head, 2);
-	add_dnodeint_end(&head, 3);
-	add_dnodeint_end(&head, 4);
-	add_dnodeint_end(&head, 98);
-	add_dnodeint_end(&head, 402);
-	add_dnodeint_end(&head, 1024);
-	print_dlistint(head);
-	node = get_dnodeint_at_index(head, 5);
-	printf("%d\n", node->n);
-	free_dlistint(head);
-	head = NULL;
-	return (EXIT_SUCCESS);
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *nya;
+
+	while (head != NULL)
+	{
+		nya = head;
+		head = head->next;
+		free(nya);
+	}
 }
