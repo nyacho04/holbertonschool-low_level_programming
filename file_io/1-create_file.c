b19830c3cf23bf86fb1427706ebb7a524fdc2060
nyacho04 <dnachod83@gmail.com>
2024-12-08 23:49:19 -0300

    create_file and add text_content parameter`

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -7,11 +7,45 @@
 #include <fcntl.h>
 #include <unistd.h>
 /**
-* read_textfile - lee un archivo de texto
-* @filename: nombre del archivo
-* @letters: cantidad de letras
-* Return: el texto
+* create_file - creates a file
+*
+* @filename: name of the file to be created
+*
+* @text_content: content of the file
+*
+* Return: 1 on success, -1 on failure
 */
 
-ssize_t read_textfile(const char *filename, size_t letters)
+int create_file(const char *filename, char *text_content)
+{
+	int fd, wr;
+	ssize_t len = 0;
 
+	if (!filename)
+	{
+		return (-1);
+	}
+	if (text_content)
+	{
+		while (text_content[len])
+		{
+			len++;
+		}
+	}
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	if (text_content)
+	{
+		wr = write(fd, text_content, len);
+		if (wr == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
