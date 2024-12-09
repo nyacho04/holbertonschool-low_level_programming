b053b69b61092dff86cd9e23379e3092e64146db
nyacho04 <dnachod83@gmail.com>
2024-12-08 23:37:37 -0300

    `Update read_textfile function to include missing includes and handle edge cases`

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,49 +1,60 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/uio.h>
 #include <fcntl.h>
 #include <unistd.h>
 /**
-* read_textfile - reads a file and prints to stdout
-*
-* @filename: name of the file to be read
-*
-* @letters: number of letters to be printed from the file
-*
-* Return: number of characters printed
+* read_textfile - lee un archivo de texto
+* @filename: nombre del archivo
+* @letters: cantidad de letras
+* Return: el texto
 */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, r, i;
-	char *tmp;
-	ssize_t ret;
+    char *tmp;
+    int opn;
+    ssize_t rd, wr;
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-	{
-	return (-1);
-	}
-	tmp = malloc(letters);
-	if (tmp == NULL)
-	{
-	return (-1);
-	}
-	r = read(fd, tmp, letters);
-	if (r == -1)
-	{
-		free(tmp);
-		return (-1);
-	}
-	ret = r;
-	i = write(STDOUT_FILENO, tmp, r);
-	if (i == -1)
-	{
-		free(tmp);
-		return (-1);
-	}
-	free(tmp);
-	return (ret);
+    if (!filename || letters == 0)
+    {
+        return (0);
+    }
+
+    tmp = malloc(sizeof(char) * letters);
+    if (!tmp)
+    {
+        return (0);
+    }
+
+    opn = open(filename, O_RDONLY);
+    if (opn == -1)
+    {
+        free(tmp);
+        return (0);
+    }
+
+    rd = read(opn, tmp, letters);
+    if (rd == -1)
+    {
+        free(tmp);
+        close(opn);
+        return (0);
+    }
+
+    wr = write(STDOUT_FILENO, tmp, rd);
+    if (wr == -1)
+    {
+        free(tmp);
+        close(opn);
+        return (0);
+    }
+
+    free(tmp);
+    close(opn);
+
+    return (rd);
 }
--- file_io/main.h
+++ file_io/main.h
@@ -3,6 +3,13 @@
 
 #include <stddef.h>
 #include <sys/types.h>
+#include <stddef.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 
